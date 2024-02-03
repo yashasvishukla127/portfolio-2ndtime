0fa360cf5c33671c7cf5e24fda5c81820c1969be
reshikaalearnig <reshikashukla127@gmail.com>
2024-02-03 09:30:58 +0530

    complete header and experience section error free

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,23 @@ type Props = {}
 function Header({}: Props) {
   return (
     <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
+      <motion.div
+        initial={{
+          x:-500,
+          opacity:0,
+          scale:0.2
+      }}
+      animate={{
+        x:0,
+        opacity:1,
+        scale:1
+      }}
+
+      transition={{
+        duration:1.3
+      }}
+      
+        className='flex flex-row items-center'>
         <SocialIcon url='https://github.com/yashasvishukla127'
             fgColor='#6e5494' bgColor='transparent'
             className='hover:cursor-pointer hover:scale-150 transition-transform duration-300'/>
@@ -29,20 +45,37 @@ function Header({}: Props) {
             className="hover:scale-150 absolute transform transition-transform duration-300">
             <Image src={hashnode} alt='image' className=" relative top-3 justify-center items-center h-6 w-6 left-2  "/>
         </SocialIcon>       
-      </div> 
+      </motion.div> 
 
-      <div className='flex flex-row items-center text-gray-500'>
+      <motion.div
+        initial={{
+          x:500,
+          opacity:0,
+          scale:0.2
+        }}
+        animate={{
+          x:0,
+          opacity:1,
+          scale:1
+          
+        }}
+        
+        transition={{
+          duration:1.3
+        }}
+      
+        className='flex flex-row items-center hover:scale-150 transform transition-transform duration-300 '>
           <SocialIcon
             className='cursor-pointer'
             url='mailto:yashshukla127@gmail.com?cc=opportunity for frontend devlopment'
             network='email'
             fgColor='gray'
             bgColor='transparent'/>
-           <p className='uppercase hidden md:inline-flex text-sm'>Get in Touch</p>
+           {/* <p className='uppercase hidden md:inline-flex text-sm'>Get in Touch</p> */}
 
     
   
-      </div> 
+      </motion.div> 
 
     </header>
 
