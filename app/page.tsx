373274d4e619b72b83ce28347ec36198b9600131
reshikaalearnig <reshikashukla127@gmail.com>
2024-02-03 08:54:46 +0530

    2nd coomit sepllling mistak

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Head from "next/head";
 import Header from "@/components/Header";
-import ExperienceReact from "@/components/experienceReact";
+import ExperienceReact from "@/components/ExperienceReact";
 
 export default function Home() {
   return (
