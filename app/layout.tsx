c355ad3ec34e6d545bb9180c9d7f7f63b26c7d98
reshikaalearnig <reshikashukla127@gmail.com>
2024-02-04 10:39:46 +0530

    made some beautification of projects photos

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} object-contain bg-cover text-slate-400 h-screen
-    snap-y snap-mandatory overflow-scroll z-0 bg-gradient-to-r from-gray-400 to-gray-100`}>
+    snap-y snap-mandatory overflow-scroll z-0  bg-gradient-to-r from-gray-400 to-gray-100`}>
         {children}
       </body>
     </html>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,13 @@ type Props = {}
 
 const Projects = (props: Props) => {
   return (
-    <div className='h-screen flex flex-col relative'>
+    <motion.div initial={{x:-200,opacity:0,}}
+    transition={{duration:0.8}}whileInView={{x:0,opacity:1,}}
+     className='h-screen flex flex-col relative text-left md:flex-row max-w-full px-10 
+    justify-evenly mx-auto items-center'>
+        
+        <h3 className='text-4xl font-extrabold absolute top-24 tracking-[12px] text-[#161A30]'> PROJECTS</h3>
+
         <div className='w-full flex absolute space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
             <motion.div className='flex-none'
               whileHover={{ scale: 1.05 }}
@@ -27,10 +33,20 @@ const Projects = (props: Props) => {
                 tech1img={"/next.svg"} tech2img={"/tailwind-css.svg"} tech3img={"/typescript.png"}
                 date='jan24-feb24'/>
             </motion.div>
+
+            <motion.div className='flex-none'
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }} >
+                <ProjectCard name='portfolio'
+                link='https://portfolio-2ndtime-9az6yr2m4-yashasvi-shuklas-projects.vercel.app'
+                image='/programmer.jpg' tech1="next" tech2="tailwind" tech3="typescript"
+                tech1img={"/next.svg"} tech2img={"/tailwind-css.svg"} tech3img={"/typescript.png"}
+                date='jan24-feb24'/>
+            </motion.div>
                 
                            
         </div>
-    </div>
+    </motion.div>
   )
 }
 
