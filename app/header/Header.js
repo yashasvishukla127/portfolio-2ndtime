6bb9dc6173a34acca7e0c72e011d9c1a7a633b92
reshikaalearnig <reshikashukla127@gmail.com>
2024-02-19 07:22:40 +0530

    4th deploy erro try

--- app/header/Header.js
+++ app/header/Header.js
@@ -1,16 +1,19 @@
-
+"use client";
 import React from "react";
 import styles from "./style.module.scss";
 import { useState } from "react";
 
-export default function header() {
+export default function Header() {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <>
-      <div onClick={()=> setIsActive(!isActive)} className={styles.button}>
-              <div className={`${styles.burger} ${isActive ? styles.burgerActive:""}` }>
-                  
+      <div className={styles.header} >
+
+        <div onClick={()=> setIsActive(!isActive)} className={styles.button}>
+                <div className={`${styles.burger} ${isActive ? styles.burgerActive:""}` }>
+                    
+          </div>
         </div>
       </div>
     </>
